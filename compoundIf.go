ff95ff4b512c410b5fb5278810a452e5b7d1cddf
vinnow98 <evinwoon@gmail.com>
2022-04-01 18:03:12 +0800

    Completed!

--- compoundIf.go
+++ compoundIf.go
@@ -1,5 +1,28 @@
 package main
 
+import "fmt"
+
+func leapYearCalculator(year int) bool {
+	var leapYear bool
+	if year%4 == 0 {
+		if year%100 == 0 {
+			if year%400 == 0 {
+				leapYear = true
+			} else {
+				leapYear = false
+			}
+		} else {
+			leapYear = true
+		}
+	} else {
+		leapYear = false
+	}
+	return leapYear
+}
 func main() {
-	//Insert your code here
+	var year int
+
+	fmt.Println("What year is it")
+	fmt.Scanln(&year)
+	fmt.Println(year, "being a leap year is", leapYearCalculator(year))
 }
